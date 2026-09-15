01290b648ca811d8d6004002d204c05a62555590
Võ Tấn Thành <votanthanh32004@gmail.com>
2026-09-15 16:56:26 +0700

    init project

---
+++ aspnet-core/src/Demo.Core/DemoConsts.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Debugging;
+
+namespace Demo;
+
+public class DemoConsts
+{
+    public const string LocalizationSourceName = "Demo";
+
+    public const string ConnectionStringName = "Default";
+
+    public const bool MultiTenancyEnabled = true;
+
+
+    /// <summary>
+    /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+    /// </summary>
+    public static readonly string DefaultPassPhrase =
+        DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "243264f862264842beaa6062b98e1992";
+}
